1b9fb9a71ea1c49c6b375d848e7b1d22d9bbf825
Kai-Shiang (Kris) Yu <76201473+kaball0808@users.noreply.github.com>
2022-05-18 20:11:05 -0400

    Time: 3 ms (68.23%), Space: 16 MB (15.86%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> curr={};
+        backtrack(0,nums,curr);
+        return ans;
+    }
+    
+    void backtrack(int index, vector<int>& nums, vector<int> curr){
+        if(index>=nums.size()){
+            ans.push_back(curr);
+            return;
+        }
+        curr.push_back(nums[index]);
+        backtrack(index+1,nums,curr);
+        curr.pop_back();
+        backtrack(index+1,nums,curr);
+    }
+};
